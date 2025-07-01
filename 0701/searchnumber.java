5a2df8ca9de2d103cea851f2de9aa349a9af30ed
y921031 <411630261@o365.tku.edu.tw>
2025-07-01 11:18:29 +0800

    work

--- 0701/searchnumber.java
+++ 0701/searchnumber.java
@@ -7,29 +7,13 @@ public class searchnumber {
 
         System.out.print("請輸入要查找的數字:");
         int x = sc.nextInt();
-
-        int left = 0;
-        int right = s.length - 1;
         boolean found = false;
 
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            System.out.println("比對中: x = "+x+"，中間元素 = "+ s[mid]);
-
-            if (s[mid] == x) {
+        for (int num : s) {
+            if (num == x) {
                 found = true;
                 break;
-            } else if (s[mid] < x){
-                left = mid + 1;
-            } else {
-                right = mid - 1;
             }
-        }
-
-        if (found) {
-            System.out.println("Yes");
-        } else {
-            System.out.println("No");
         }
     }
 }
